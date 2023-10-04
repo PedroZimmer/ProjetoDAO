5a64220064f891357e40c5eebcc5382d040cba58
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-10-04 14:09:07 -0300

    aula 280 DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 
         Seller seller = new Seller(21, "Bobo", "Bobo@", new Date(), 3000.0, obj);
         System.out.println(seller);
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDAO();
 
 
     }
