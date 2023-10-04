c3ff3f4feae45db52779c907ea299272c94b8e2e
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-10-04 14:47:29 -0300

    aula 282 reutilizando

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,10 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-
-
         SellerDao sellerDAO = DaoFactory.createSellerDAO();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDAO.findById(3);
-
         System.out.println(seller);
 
 
