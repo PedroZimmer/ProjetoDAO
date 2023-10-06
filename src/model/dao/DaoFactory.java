60ddd220180a10d42f04c95337e76dda7dfd6814
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-10-06 14:26:46 -0300

    daoFactory createDepartmentDao

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.model.dao.impl.DepartmentDaoJDBC;
 import model.dao.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDAO() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDAO() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 
 }
