5e699bc7628dc1138d0c573ca30f781f8c90e622
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-10-06 14:03:44 -0300

    aula 286 update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,13 @@ public class Program {
         sellerDAO.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDAO.findById(1);
+        seller.setName("Klebinho");
+        sellerDAO.update(seller);
+        System.out.println("Update completed!");
+
+
 
     }
 
